192cd323c035d8813abc1d31559c863504ddbdd7
Rathinkumar23 <131570099+Rathinkumar23@users.noreply.github.com>
2023-05-16 16:51:10 +0530

    Add files via upload

--- week2/day2/DeleteLead.java
+++ week2/day2/DeleteLead.java
@@ -22,7 +22,14 @@ driver.findElement(By.name("phoneCountryCode")).clear();
 driver.findElement(By.name("phoneCountryCode")).sendKeys("+91");
 driver.findElement(By.name("phoneNumber")).sendKeys("8344851575");
 driver.findElement(By.linkText("Find Leads")).click();
-
+Thread.sleep(2000);
+driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+driver.findElement(By.xpath("//a[@class='subMenuButtonDangerous']")).click();
+driver.findElement(By.linkText("Find Leads")).click();
+driver.findElement(By.xpath("//input[@id='ext-gen246']")).sendKeys("10035");
+driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+Thread.sleep(2000);
+driver.close();
 
 
 
